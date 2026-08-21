90b8647d589cfc0e0ca9fcafbaf532195ef8ae05
JOXWORK <joxwork1995@gmail.com>
2026-08-21 17:30:55 +0500

    integrate: keep method for rate limit test endpoint

--- application/api/v1/authentication_test/views.py
+++ application/api/v1/authentication_test/views.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from core.authentication.rate_limit_guard import rate_limit_guard
 from fastapi import APIRouter, Depends
 
 from api.dependencies.auth import fastapi_current_user
@@ -18,5 +19,6 @@ async def is_user_valid(user: User = Depends(fastapi_current_user)):
 
 
 @router.post("/rate-limit")
+@rate_limit_guard.keep_rate_limit()
 async def rate_limit_endpoint(user: User = Depends(fastapi_current_user)):
     return {"message": "succsessful"}
