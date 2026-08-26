144abc548ea818658f953624f6ad36daa479ff90
JOXWORK <joxwork1995@gmail.com>
2026-08-26 13:49:49 +0500

    integrate: modified reset password module

--- application/api/v1/authentication/authentication.py
+++ application/api/v1/authentication/authentication.py
@@ -5,6 +5,7 @@ from fastapi.security import HTTPBearer
 
 from api.dependencies.auth.backend import auth_backend
 from api.dependencies.auth.fastapi_users_instance import fastapi_users
+from api.dependencies.auth.routers.reset_own import get_reset_password_router
 from api.dependencies.auth.routers.verify_own import get_verify_router
 
 http_bearer = HTTPBearer(auto_error=False)
@@ -19,11 +20,11 @@ router.include_router(
 )
 
 router.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
+    fastapi_users.get_register_router(user_schema=UserRead, user_create_schema=UserCreate),
 )
 
 router.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
+    fastapi_users.get_users_router(user_schema=UserRead, user_update_schema=UserUpdate),
 )
 
 router.include_router(
@@ -31,5 +32,5 @@ router.include_router(
 )
 
 router.include_router(
-    fastapi_users.get_reset_password_router(),
+    get_reset_password_router(get_user_manager=fastapi_users.get_user_manager),
 )
