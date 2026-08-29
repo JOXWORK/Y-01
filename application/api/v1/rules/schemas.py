26c9bdc417f576de6658e7a71f69d5f6ec22065e
JOXWORK <joxwork1995@gmail.com>
2026-08-29 14:03:59 +0500

    remove/replace: excessive pydantic schema, replace by simple dict

--- application/api/v1/rules/schemas.py
+++ application/api/v1/rules/schemas.py
@@ -5,10 +5,6 @@ class ModerationRulesSchema(BaseModel):
     rules: dict[str, str]  # rule: action
 
 
-class TaskIDSchema(BaseModel):
-    task_id: str
-
-
 class TaskReadySuccessResult(BaseModel):
     is_ready: bool
     successful: bool
--- application/api/v1/rules/views.py
+++ application/api/v1/rules/views.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, Depends
 from api.dependencies.auth.fastapi_users_instance import fastapi_current_user
 
 from . import crud
-from .schemas import ModerationRulesSchema, TaskIDSchema, TaskReadySuccessResult
+from .schemas import ModerationRulesSchema, TaskReadySuccessResult
 
 if TYPE_CHECKING:
     from core.models.user import User
@@ -20,18 +20,18 @@ router = APIRouter()
 async def moderation_rules_set(
     rules_schema: ModerationRulesSchema,
     user: User = Depends(fastapi_current_user),
-) -> TaskIDSchema:
+) -> dict[str, str]:
     task_id = await crud.kick_write_rules_task(
         user_id=user.id,
         rules_schema=rules_schema,
     )
 
-    return TaskIDSchema(task_id=task_id)
+    return {"task_id": task_id}
 
 
 @router.post("/result")
 async def moderation_rules_result(
-    task_id_schema: TaskIDSchema,
+    task_id_schema: str,
     user: User = Depends(fastapi_current_user),
 ) -> TaskReadySuccessResult:
     result = await crud.get_task_result(task_id=task_id_schema.task_id)
