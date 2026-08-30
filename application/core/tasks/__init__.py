d6ebb58f6516d6b0687045c85fab19c0721f36d8
JOXWORK <joxwork1995@gmail.com>
2026-08-30 15:09:24 +0500

    rename: task file write rules into db

--- application/core/tasks/__init__.py
+++ application/core/tasks/__init__.py
@@ -1,9 +1,11 @@
 # ruff: noqa: F401, I001
 
 from .hello_world.for_loop_task_example import for_loop_task_example_task
-from .moderation_rules.write_moderation_rules_db import write_moderation_rules_db_task
+from .moderation_rules.set_moderation_rules_db import set_moderation_rules_db_task
+from .moderation_rules.get_moderation_rules_db import get_moderation_rules_db_task
 
 __all__ = (
     "for_loop_task_example_task",
-    "write_moderation_rules_db_task",
+    "set_moderation_rules_db_task",
+    "get_moderation_rules_db_task",
 )
