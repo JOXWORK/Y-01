ae6590a2ef0cc42776e6b6cc738761fa21a79fe7
JOXWORK <joxwork1995@gmail.com>
2026-08-31 14:56:26 +0500

    add: llm message send task into init file

--- application/core/tasks/__init__.py
+++ application/core/tasks/__init__.py
@@ -3,9 +3,11 @@
 from .hello_world.for_loop_task_example import for_loop_task_example_task
 from .moderation_rules.set_moderation_rules_db import set_moderation_rules_db_task
 from .moderation_rules.get_moderation_rules_db import get_moderation_rules_db_task
+from .message_moderation.send_message_moderation_api import send_message_moderation_api_task
 
 __all__ = (
     "for_loop_task_example_task",
     "set_moderation_rules_db_task",
     "get_moderation_rules_db_task",
+    "send_message_moderation_api_task",
 )
