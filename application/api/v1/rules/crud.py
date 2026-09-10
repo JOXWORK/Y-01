946d4cc678f6e2bcd932879cfa697a707cd0c2b9
JOXWORK <joxwork1995@gmail.com>
2026-09-10 16:09:54 +0500

    create: get human readable moderation rules endpoint

--- application/api/v1/rules/crud.py
+++ application/api/v1/rules/crud.py
@@ -1,4 +1,5 @@
 from core.schemas.moderation_rules import ModerationRulesSchema
+from core.tasks.moderation_rules.get_human_readable_moderation_rules_db import get_human_readable_moderation_rules_db_task
 from core.tasks.moderation_rules.get_moderation_rules_db import get_moderation_rules_db_task
 from core.tasks.moderation_rules.set_moderation_rules_db import set_moderation_rules_db_task
 
@@ -16,5 +17,11 @@ async def create_request(user_id: int, rules_schema: ModerationRulesSchema) -> T
 
 async def get_request(user_id: int) -> TaskIDSchema:
     task = await get_moderation_rules_db_task.kiq(user_id)
+
+    return TaskIDSchema(task_id=task.task_id)
+
+
+async def get_human_readable_request(user_id: int) -> ModerationRulesSchema:
+    task = await get_human_readable_moderation_rules_db_task.kiq(user_id)
 
     return TaskIDSchema(task_id=task.task_id)
