26d68899b8ea930848f4f2494bc9ae9231a990f1
JOXWORK <joxwork1995@gmail.com>
2026-09-11 16:12:46 +0500

    remove: rules api exceptions module

--- application/api/v1/rules/exceptions.py
+++
@@ -1,9 +0,0 @@
-from core.http.error_details._rules import RulesErrorDetails
-from fastapi import HTTPException, status
-
-
-def http_wrong_task_result():
-    return HTTPException(
-        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-        detail=RulesErrorDetails.WRONG_TASK_RESULT,
-    )
