0c22487722470324853a58810ad811613e775fe7
JOXWORK <joxwork1995@gmail.com>
2026-09-14 21:06:04 +0500

    add: get mod rules micro task into 'tasks' init file module

--- application/core/tasks/__init__.py
+++ application/core/tasks/__init__.py
@@ -1,13 +1,19 @@
 # ruff: noqa: F401, I001
 
+# Tasks
 from .hello_world.for_loop_task_example import for_loop_task_example_task
 from .moderation_rules.set_moderation_rules_db import set_moderation_rules_db_task
 from .moderation_rules.get_moderation_rules_db import get_moderation_rules_db_task
 from .message_moderation.send_message_moderation_api import send_message_moderation_api_task
+
+
+# Micro tasks
+from .micro_tasks.micro_get_moderation_rules import get_moderation_rules_micro_task
 
 __all__ = (
     "for_loop_task_example_task",
     "set_moderation_rules_db_task",
     "get_moderation_rules_db_task",
     "send_message_moderation_api_task",
+    "get_moderation_rules_micro_task",
 )
