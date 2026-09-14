60e6d43453b0de8883a5fe472bad87fcdf17f3d7
JOXWORK <joxwork1995@gmail.com>
2026-09-14 21:08:12 +0500

    integrate: get mod rules micro task into tasks

--- application/core/tasks/message_moderation/send_message_moderation_api.py
+++ application/core/tasks/message_moderation/send_message_moderation_api.py
@@ -1,20 +1,19 @@
 from core.config import settings
 from core.llm_response_journal.journal import LLMResponseJournalWriteError, llm_response_journal
-from core.models.db_attach import db_attach
-from core.models.moderation_rule import ModerationRule
 from core.openai_client.client import openai_client
 from core.promts.message_moderation import system_promt
 from core.schemas.moderation_response import ModerationLLMResponseSchema
 from core.schemas.task_response import TaskResponseSchema
 from core.taskiq.broker import broker
+from core.taskiq.task_messages import TaskResponseMessages, create_message
 from core.taskiq.task_runtime_logger import task_runtime_logger
+from core.tasks.micro_tasks.micro_get_moderation_rules import get_moderation_rules_micro_task
 from openai import OpenAIError
 from pydantic import ValidationError
-from sqlalchemy import select
 from taskiq import Context, TaskiqDepends
 
 
-async def generate_user_promt(message: str, rules: dict):
+def generate_user_promt(message: str, rules: dict):
     return f"""
     <rules>
     {rules}
@@ -26,19 +25,6 @@ async def generate_user_promt(message: str, rules: dict):
     """
 
 
-async def get_moderation_rules(user_id: int):
-    async with db_attach.session_factory() as session:
-        query = select(ModerationRule).where(ModerationRule.user_id == user_id)
-        sqla_result = await session.execute(query)
-
-        moderation_rule = sqla_result.scalar_one_or_none()
-
-        if moderation_rule is None:
-            return None
-
-        return moderation_rule.rules
-
-
 @broker.task
 async def send_message_moderation_api_task(
     message: str,
@@ -48,36 +34,44 @@ async def send_message_moderation_api_task(
     response = TaskResponseSchema(successful=False, content=None)
 
     try:
-        rules = await get_moderation_rules(user_id)
-        USER_PROMT = await generate_user_promt(
-            message=message,
-            rules=rules,
-        )
+        rules_micro_task = await get_moderation_rules_micro_task.kiq(user_id=user_id)
+        rules_micro_task_result = await rules_micro_task.wait_result()
+        rules_micro_task_response = rules_micro_task_result.return_value
 
-        llm_response = await openai_client.chat.completions.create(
-            model=settings.cloud_ru_api.model,
-            messages=[
-                {
-                    "role": "developer",
-                    "content": system_promt.PROMT,
-                },
-                {
-                    "role": "user",
-                    "content": USER_PROMT,
-                },
-            ],
-        )
+        if rules_micro_task_response.successful:
+            rules = rules_micro_task_response.content["rules_numbered"]
 
-        llm_content = llm_response.choices[0].message.content.strip()
-        await llm_response_journal.write(
-            key_name=context.message.task_id,
-            content=llm_content,
-        )
+            PROMT = generate_user_promt(
+                message=message,
+                rules=rules,
+            )
 
-        response.content = ModerationLLMResponseSchema.model_validate_json(
-            llm_content,
-        ).model_dump()
-        response.successful = True
+            llm_response = await openai_client.chat.completions.create(
+                model=settings.cloud_ru_api.model,
+                messages=[
+                    {
+                        "role": "developer",
+                        "content": system_promt.PROMT,
+                    },
+                    {
+                        "role": "user",
+                        "content": PROMT,
+                    },
+                ],
+            )
+
+            llm_content = llm_response.choices[0].message.content.strip()
+            await llm_response_journal.write(
+                key_name=context.message.task_id,
+                content=llm_content,
+            )
+
+            response.content = ModerationLLMResponseSchema.model_validate_json(
+                llm_content,
+            ).model_dump()
+            response.successful = True
+        else:
+            response.content = create_message(TaskResponseMessages.RULES_NOT_FOUND)
     except OpenAIError:
         task_runtime_logger.logger.error(
             "Openai module exception",
--- application/core/tasks/moderation_rules/get_moderation_rules_db.py
+++ application/core/tasks/moderation_rules/get_moderation_rules_db.py
@@ -1,33 +1,29 @@
-from core.models import db_attach
-from core.models.moderation_rule import ModerationRule
 from core.schemas.task_response import TaskResponseSchema
 from core.taskiq.broker import broker
+from core.taskiq.task_messages import TaskResponseMessages, create_message
 from core.taskiq.task_runtime_logger import task_runtime_logger
-from sqlalchemy import select
-from sqlalchemy.exc import SQLAlchemyError
+from core.tasks.micro_tasks.micro_get_moderation_rules import get_moderation_rules_micro_task
 
 
 @broker.task
 async def get_moderation_rules_db_task(user_id: int) -> TaskResponseSchema:
     response = TaskResponseSchema(successful=False, content=None)
     try:
-        async with db_attach.session_factory() as session:
-            query = select(ModerationRule).where(ModerationRule.user_id == user_id)
-            sqla_result = await session.execute(query)
+        rules_micro_task = await get_moderation_rules_micro_task.kiq(user_id)
+        rules_micro_task_result = await rules_micro_task.wait_result()
+        rules_micro_task_response = rules_micro_task_result.return_value
 
-            moderation_rule = sqla_result.scalar_one_or_none()
+        if rules_micro_task_response.successful:
+            response_content = rules_micro_task_response.content
+            content = {}
 
-            content = {"message": "Rules not found"}  # replace by messages
-            if moderation_rule:
-                content.clear()
-
-                content.update(moderation_rule.rules)
-                content.update(moderation_rule.rules_numbered)
+            content.update(response_content["rules"])
+            content.update(response_content["rules_numbered"])
 
             response.content = content
             response.successful = True
-    except SQLAlchemyError:
-        task_runtime_logger.logger.error("SQLAlchemy exception", exc_info=True)
+        else:
+            response.content = create_message(TaskResponseMessages.RULES_NOT_FOUND)
     except Exception:
         task_runtime_logger.logger.error("Unexpected exception", exc_info=True)
 
