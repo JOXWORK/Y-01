7f955a73a3f004d06b8d9f89128cdbd5c5a1826a
JOXWORK <joxwork1995@gmail.com>
2026-09-14 20:58:13 +0500

    rename: module 'messages' -> 'task_messages'

--- application/core/taskiq/task_messages.py
+++ application/core/taskiq/task_messages.py
@@ -5,5 +5,5 @@ class TaskResponseMessages(str, Enum):
     RULES_NOT_FOUND = "Rules not found"
 
 
-def create_message(message: EnumType) -> dict[str, EnumType]:
-    return {"message": message}
+def create_message(message: EnumType) -> dict[str, str]:
+    return {"message": message.value}
